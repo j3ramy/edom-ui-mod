393ce2fa6514ffa1fdb0879db0845663d4d4cdf2
j3ramy <seidel.jaimy99@gmx.de>
2022-07-03 10:52:53 +0200

    First upload

--- src/main/java/de/j3ramy/edom/item/FivehundredEuro.java
+++
@@ -1,9 +0,0 @@
-package de.j3ramy.edom.item;
-
-import net.minecraft.item.Item;
-
-public class FivehundredEuro extends Item {
-    public FivehundredEuro(Properties properties) {
-        super(properties);
-    }
-}
--- src/main/java/de/j3ramy/edom/item/OnehundredEuro.java
+++
@@ -1,9 +0,0 @@
-package de.j3ramy.edom.item;
-
-import net.minecraft.item.Item;
-
-public class OnehundredEuro extends Item {
-    public OnehundredEuro(Properties properties) {
-        super(properties);
-    }
-}
