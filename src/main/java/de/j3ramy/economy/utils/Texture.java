fcaacc1bd5fa8624964833a8d1ea1b87fd19e7b1
j3ramy <seidel.jaimy99@gmx.de>
2022-07-14 22:01:34 +0200

    Update package and textures

--- src/main/java/de/j3ramy/economy/utils/Texture.java
+++ src/main/java/de/j3ramy/economy/utils/Texture.java
@@ -4,6 +4,6 @@ import de.j3ramy.economy.EconomyMod;
 import net.minecraft.util.ResourceLocation;
 
 public class Texture {
-    public static final ResourceLocation CLEAR_DB_BUTTON_TEX = new ResourceLocation(EconomyMod.MOD_ID, "textures/gui/elements/clear_db_gui.png");
-    public static final ResourceLocation DELETE_BUTTON_TEX = new ResourceLocation(EconomyMod.MOD_ID, "textures/gui/elements/clear_db_gui.png");
+    public static final ResourceLocation CLEAR_DB_BUTTON_TEX = new ResourceLocation(EconomyMod.MOD_ID, "textures/gui/widgets/clear_db_gui.png");
+    public static final ResourceLocation DELETE_BUTTON_TEX = new ResourceLocation(EconomyMod.MOD_ID, "textures/gui/widgets/trashcan_button_gui.png");
 }
